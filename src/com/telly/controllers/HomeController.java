95e41c027630f9446cdb63a18d3c4251b4ac4353
Wael <khelilwael@gmail.com>
2018-11-23 22:40:42 +0100

    Add showloging methods security file config

--- src/com/telly/controllers/HomeController.java
+++ src/com/telly/controllers/HomeController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
+
 	@RequestMapping("/")
 	public String showHome() {
 		
